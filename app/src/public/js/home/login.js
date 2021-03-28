9a5ce57a9ff7e7729a3a8d048143fc2540c4a223
fbwlgh07 <fbwlgh07@gmail.com>
2021-03-28 18:34:06 +0900

    login auth

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -8,8 +8,17 @@ loginBtn.addEventListener("click",login);
 
 function login(){
     const req = {
-        id : id.Value,
+        id : id.value,
         psword : psword.value,
     };
-    console.log(req);
+    
+    
+    fetch("/login",{
+        method : "POST",
+        headers:{
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(req),
+    }).then((res) => res.json())
+      .then(console.log);
 }
