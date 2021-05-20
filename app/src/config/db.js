6dc3b3f67f44321031c0ac4bcfdb79f901a03f8b
fbwlgh07 <fbwlgh07@gmail.com>
2021-05-20 23:00:51 +0900

    AWS RDS and dotenv assgin

---
+++ app/src/config/db.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql");
+
+const db = mysql.createConnection({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PSWORD,
+    database : process.env.DB_DATABASES,
+});
+
+db.connect();
+
+module.exports = db;
